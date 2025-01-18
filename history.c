885c6c307dcd2eabdda130860a285568b38eb30d
hugog <hugo.ghesquier59@gmail.com>
2025-01-18 11:30:50 +0100

    chore: remove CMakeFiles directory from GitHub

--- history.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by hugog on 03/01/2025.
-//
-
-#include "history.h"
--- history.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by hugog on 03/01/2025.
-//
-
-#ifndef HISTORY_H
-#define HISTORY_H
-
-#endif //HISTORY_H
